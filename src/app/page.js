1986f04d663e028ffa6ea56da41acaa735ba7bd3
Buyung Hardiansyah <buyung.hardiansyah@gmail.com>
2025-01-08 18:06:08 +0700

    setup layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,227 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <main>
+      <div className="text-slate-700">
+        <header className="px-4 py-8 md:px-6 md:py-10 lg:py-12">
+          <div className="mx-auto w-full max-w-3xl">
+            <div className="grid grid-cols-1 justify-items-center gap-20">
+              <nav>
+                <ul className="flex flex-wrap justify-center gap-10">
+                  <li className="font-semibold tracking-tight text-slate-800">
+                    <a href="/">Articles</a>
+                  </li>
+                  <li className="font-semibold tracking-tight text-slate-800">
+                    <a href="/about">About</a>
+                  </li>
+                  <li className="font-semibold tracking-tight text-slate-800">
+                    <a href="">Contact Me</a>
+                  </li>
+                </ul>
+              </nav>
+              <div className="px-4">
+                <div className="grid max-w-lg grid-cols-1 justify-items-center gap-8">
+                  <a tabIndex={-1} href="/">
+                    <div className="relative h-40 w-40 overflow-hidden rounded-full bg-slate-300">
+                      <img
+                        alt="A photo of Margaret Smith (fictional)."
+                        loading="lazy"
+                        decoding="async"
+                        data-nimg="fill"
+                        className="object-cover"
+                        style={{
+                          position: "absolute",
+                          height: "100%",
+                          width: "100%",
+                          left: 0,
+                          top: 0,
+                          right: 0,
+                          bottom: 0,
+                          color: "transparent"
+                        }}
+                        sizes="100vw"
+                        src="https://images.prismic.io/nextjs-starter-prismic-blog/af1ae0b2-2b21-4a71-be78-cee3930b674b_mathilde-langevin-PaCzyxEcqiw-unsplash.jpg?auto=compress%2Cformat&rect=0%2C1000%2C4000%2C4000&w=3840&fit=max"
+                      />
+                    </div>
+                  </a>
+                  <div className="grid grid-cols-1 gap-2 text-center">
+                    <h1 className="font-sans font-semibold tracking-tighter text-slate-800 text-3xl md:text-4xl">
+                      <a href="/">Margaret Smith</a>
+                    </h1>
+                    <p className="font-serif text-2xl italic leading-normal tracking-tight text-slate-500">
+                      Writer at The Daily Times spending all of my free time writing
+                      stories
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </header>
+        <main>
+          <div className="px-4 py-8 md:px-6 md:py-10 lg:py-12">
+            <div className="mx-auto w-full max-w-6xl">
+              <ul className="grid grid-cols-1 gap-16">
+                <li className="grid grid-cols-1 items-start gap-6 md:grid-cols-3 md:gap-8">
+                  <a tabIndex={-1} href="/articles/hiking-through-the-woods">
+                    <div className="aspect-h-3 aspect-w-4 relative bg-gray-100">
+                      <img
+                        alt="list 1"
+                        loading="lazy"
+                        decoding="async"
+                        data-nimg="fill"
+                        className="object-cover"
+                        sizes="100vw"
+                        src="https://images.prismic.io/nextjs-starter-prismic-blog/fb66c550-06ee-406b-8be3-06ac639f7bb0_laura-kennedy-VVUOodsIwj4-unsplash.jpg?auto=compress%2Cformat&rect=0%2C1404%2C3744%2C2808&w=3840&fit=max"
+                      />
+                    </div>
+                  </a>
+                  <div className="grid grid-cols-1 gap-3 md:col-span-2">
+                    <h2 className="font-sans font-semibold tracking-tighter text-slate-800 text-3xl md:text-4xl">
+                      <a href="/articles/hiking-through-the-woods">
+                        Hiking through the woods
+                      </a>
+                    </h2>
+                    <p className="font-serif italic tracking-tighter text-slate-500">
+                      Apr 12, 2022
+                    </p>
+                    <p className="font-serif leading-relaxed md:text-lg md:leading-relaxed">
+                      This is Rich Text, which includes both external links and
+                      links to internal documents. Links should be handled
+                      intelligently or everything might break. Don't forget about
+                      media, too! Do your best to render images using an HTML
+                      Serializer. As you know hiking can be a very fulfilling orem
+                      ipsum dolor…
+                    </p>
+                  </div>
+                </li>
+                <li className="grid grid-cols-1 items-start gap-6 md:grid-cols-3 md:gap-8">
+                  <a tabIndex={-1} href="/articles/beach-resort-vacation">
+                    <div className="aspect-h-3 aspect-w-4 relative bg-gray-100">
+                      <img
+                        alt="link 2"
+                        loading="lazy"
+                        decoding="async"
+                        data-nimg="fill"
+                        className="object-cover"
+                        sizes="100vw"
+                        srcSet="https://images.prismic.io/nextjs-starter-prismic-blog/599891e1-cfd4-4c9d-8c21-a498c1dcac16_alex-holt-Qu-VEc0hLS4-unsplash.jpg?auto=compress%2Cformat&rect=0%2C1560%2C4160%2C3120&w=640&fit=max 640w, https://images.prismic.io/nextjs-starter-prismic-blog/599891e1-cfd4-4c9d-8c21-a498c1dcac16_alex-holt-Qu-VEc0hLS4-unsplash.jpg?auto=compress%2Cformat&rect=0%2C1560%2C4160%2C3120&w=750&fit=max 750w, https://images.prismic.io/nextjs-starter-prismic-blog/599891e1-cfd4-4c9d-8c21-a498c1dcac16_alex-holt-Qu-VEc0hLS4-unsplash.jpg?auto=compress%2Cformat&rect=0%2C1560%2C4160%2C3120&w=828&fit=max 828w, https://images.prismic.io/nextjs-starter-prismic-blog/599891e1-cfd4-4c9d-8c21-a498c1dcac16_alex-holt-Qu-VEc0hLS4-unsplash.jpg?auto=compress%2Cformat&rect=0%2C1560%2C4160%2C3120&w=1080&fit=max 1080w, https://images.prismic.io/nextjs-starter-prismic-blog/599891e1-cfd4-4c9d-8c21-a498c1dcac16_alex-holt-Qu-VEc0hLS4-unsplash.jpg?auto=compress%2Cformat&rect=0%2C1560%2C4160%2C3120&w=1200&fit=max 1200w, https://images.prismic.io/nextjs-starter-prismic-blog/599891e1-cfd4-4c9d-8c21-a498c1dcac16_alex-holt-Qu-VEc0hLS4-unsplash.jpg?auto=compress%2Cformat&rect=0%2C1560%2C4160%2C3120&w=1920&fit=max 1920w, https://images.prismic.io/nextjs-starter-prismic-blog/599891e1-cfd4-4c9d-8c21-a498c1dcac16_alex-holt-Qu-VEc0hLS4-unsplash.jpg?auto=compress%2Cformat&rect=0%2C1560%2C4160%2C3120&w=2048&fit=max 2048w, https://images.prismic.io/nextjs-starter-prismic-blog/599891e1-cfd4-4c9d-8c21-a498c1dcac16_alex-holt-Qu-VEc0hLS4-unsplash.jpg?auto=compress%2Cformat&rect=0%2C1560%2C4160%2C3120&w=3840&fit=max 3840w"
+                        src="https://images.prismic.io/nextjs-starter-prismic-blog/599891e1-cfd4-4c9d-8c21-a498c1dcac16_alex-holt-Qu-VEc0hLS4-unsplash.jpg?auto=compress%2Cformat&rect=0%2C1560%2C4160%2C3120&w=3840&fit=max"
+                      />
+                    </div>
+                  </a>
+                  <div className="grid grid-cols-1 gap-3 md:col-span-2">
+                    <h2 className="font-sans font-semibold tracking-tighter text-slate-800 text-3xl md:text-4xl">
+                      <a href="/articles/beach-resort-vacation">
+                        Beach resort vacation
+                      </a>
+                    </h2>
+                    <p className="font-serif italic tracking-tighter text-slate-500">
+                      Mar 22, 2022
+                    </p>
+                    <p className="font-serif leading-relaxed md:text-lg md:leading-relaxed">
+                      Massa sapien faucibus et molestie ac feugiat sed lectus.
+                      Dignissim cras tincidunt lobortis feugiat vivamus at augue.
+                      Pharetra pharetra massa massa ultricies. Ut morbi tincidunt
+                      augue interdum. Volutpat odio facilisis mauris sit amet massa
+                      vitae. In dictum non consectetur a erat nam. Pulvinar…
+                    </p>
+                  </div>
+                </li>
+                <li className="grid grid-cols-1 items-start gap-6 md:grid-cols-3 md:gap-8">
+                  <a tabIndex={-1} href="/articles/perilous-yoga">
+                    <div className="aspect-h-3 aspect-w-4 relative bg-gray-100">
+                      <img
+                        alt="link 3"
+                        loading="lazy"
+                        decoding="async"
+                        data-nimg="fill"
+                        className="object-cover"
+                        sizes="100vw"
+                        srcSet="https://images.prismic.io/nextjs-starter-prismic-blog/80cb000f-544e-45e1-b67f-fe35c6d42725_jess-harper-sunday-MBwPzT_vbqo-unsplash.jpg?auto=compress%2Cformat&rect=0%2C1250%2C3333%2C2500&w=640&fit=max 640w, https://images.prismic.io/nextjs-starter-prismic-blog/80cb000f-544e-45e1-b67f-fe35c6d42725_jess-harper-sunday-MBwPzT_vbqo-unsplash.jpg?auto=compress%2Cformat&rect=0%2C1250%2C3333%2C2500&w=750&fit=max 750w, https://images.prismic.io/nextjs-starter-prismic-blog/80cb000f-544e-45e1-b67f-fe35c6d42725_jess-harper-sunday-MBwPzT_vbqo-unsplash.jpg?auto=compress%2Cformat&rect=0%2C1250%2C3333%2C2500&w=828&fit=max 828w, https://images.prismic.io/nextjs-starter-prismic-blog/80cb000f-544e-45e1-b67f-fe35c6d42725_jess-harper-sunday-MBwPzT_vbqo-unsplash.jpg?auto=compress%2Cformat&rect=0%2C1250%2C3333%2C2500&w=1080&fit=max 1080w, https://images.prismic.io/nextjs-starter-prismic-blog/80cb000f-544e-45e1-b67f-fe35c6d42725_jess-harper-sunday-MBwPzT_vbqo-unsplash.jpg?auto=compress%2Cformat&rect=0%2C1250%2C3333%2C2500&w=1200&fit=max 1200w, https://images.prismic.io/nextjs-starter-prismic-blog/80cb000f-544e-45e1-b67f-fe35c6d42725_jess-harper-sunday-MBwPzT_vbqo-unsplash.jpg?auto=compress%2Cformat&rect=0%2C1250%2C3333%2C2500&w=1920&fit=max 1920w, https://images.prismic.io/nextjs-starter-prismic-blog/80cb000f-544e-45e1-b67f-fe35c6d42725_jess-harper-sunday-MBwPzT_vbqo-unsplash.jpg?auto=compress%2Cformat&rect=0%2C1250%2C3333%2C2500&w=2048&fit=max 2048w, https://images.prismic.io/nextjs-starter-prismic-blog/80cb000f-544e-45e1-b67f-fe35c6d42725_jess-harper-sunday-MBwPzT_vbqo-unsplash.jpg?auto=compress%2Cformat&rect=0%2C1250%2C3333%2C2500&w=3840&fit=max 3840w"
+                        src="https://images.prismic.io/nextjs-starter-prismic-blog/80cb000f-544e-45e1-b67f-fe35c6d42725_jess-harper-sunday-MBwPzT_vbqo-unsplash.jpg?auto=compress%2Cformat&rect=0%2C1250%2C3333%2C2500&w=3840&fit=max"
+                      />
+                    </div>
+                  </a>
+                  <div className="grid grid-cols-1 gap-3 md:col-span-2">
+                    <h2 className="font-sans font-semibold tracking-tighter text-slate-800 text-3xl md:text-4xl">
+                      <a href="/articles/perilous-yoga">Perilous yoga</a>
+                    </h2>
+                    <p className="font-serif italic tracking-tighter text-slate-500">
+                      Jan 6, 2022
+                    </p>
+                    <p className="font-serif leading-relaxed md:text-lg md:leading-relaxed">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                      do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                      Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                      laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                      irure dolor in reprehenderit in voluptate velit esse…
+                    </p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </main>
+        <footer className="px-4 py-8 md:px-6 md:py-10 lg:py-12">
+          <div className="mx-auto w-full max-w-3xl">
+            <div className="grid grid-cols-1 justify-items-center gap-24">
+              <hr className="h-px w-1/2 border-0 bg-slate-200 md:max-w-sm" />
+              <div className="px-4">
+                <form
+                  className="grid w-full max-w-xl grid-cols-1 gap-6"
+                  action="/api/sign-up"
+                  method="post"
+                >
+                  <div className="text-center font-serif tracking-tight text-slate-500">
+                    <h2 className="font-sans font-semibold tracking-tighter text-slate-800 text-3xl md:text-4xl mb-4 last:mb-0">
+                      Follow my journey
+                    </h2>
+                    <p className="mb-4 italic last:mb-0">
+                      Get notified about updates and be the first to get early
+                      access to the new, safer, and smarter way to archive your
+                      files.
+                    </p>
+                  </div>
+                  <div className="grid grid-cols-1 gap-2">
+                    <div className="relative">
+                      <label>
+                        <span className="sr-only">Email address</span>
+                        <input
+                          type="email"
+                          placeholder="jane.doe@example.com"
+                          required=""
+                          className="w-full rounded-none border-b border-slate-200 py-3 pl-3 pr-10 text-slate-800 placeholder-slate-400"
+                          name="email"
+                        />
+                      </label>
+                      <button
+                        type="submit"
+                        className="absolute bottom-0 right-0 top-0 flex items-center justify-center px-3 text-2xl text-slate-400"
+                      >
+                        <span className="sr-only">Submit</span>
+                        <span aria-hidden="true">→</span>
+                      </button>
+                    </div>
+                    <p className="text-center text-xs tracking-tight text-slate-500">
+                      By subscribing to our newsletter you accept to receive
+                      recurring emails about our product and company
+                    </p>
+                  </div>
+                </form>
+              </div>
+              <div className="mx-auto w-full max-w-3xl text-center text-xs font-semibold tracking-tight text-slate-500">
+                Proudly published using
+                {/* */}
+                <a className="text-slate-700" href="https://prismic.io">
+                  Prismic
+                </a>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </div>
+    </main>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
   );
 }
---
+++ src/pages/about.js
@@ -0,0 +1,7 @@
+import Layout from '../app/components/layout'
+ 
+export default function About() {
+  return (
+    <h1>about</h1>
+  )
+}
